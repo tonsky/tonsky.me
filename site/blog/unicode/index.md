92bcb7ef7fcd00fb187aab5be3d064a3b7cee751
Nikita Prokopov <niki@tonsky.me>
2023-09-13 01:50:56 +0200

    Committed old generated site

---
+++ site/blog/unicode/index.md
@@ -0,0 +1,58 @@
+Exactly 20 years has passed since Joel Spolsky [wrote his version](https://www.joelonsoftware.com/2003/10/08/the-absolute-minimum-every-software-developer-absolutely-positively-must-know-about-unicode-and-character-sets-no-excuses/), so it’s about time we have an update.
+
+In 2003, the question was: what encoding does this string/text/file use? Joel even went as far as to declare: there’s no such thing as plain text!
+
+Luckily for us, in the last 20 years Unicode has finally won: in 99,9%, your plain text it’s UTF-8. Hooray!
+
+Hooray?
+
+# What is Unicode
+
+- 21-bit
+- 17 planes
+- Plane = 216 = 65 536 code points
+- Plane 0
+  - Basic Multilingual Plane
+  - Все нужные языки
+- Plane 1
+  - Supplementary Multilingual Plane
+  - Linear B, Egyptian hieroglyphs, cuneiform, Shavian, Deseret, Osage, Warang Citi, Adlam, Wancho and Toto.
+  - musical notation; mathematical alphanumerics; shorthands; Emoji and other pictographic sets; and game symbols for playing cards, mahjong, and dominoes.
+- Planes 2-3
+  - CJK Unified Ideographs
+- Planes 4-14
+  - Unallocated or barely used
+- Planes 15-16
+  - Private use area
+
+# Проблемы
+
+- UTF-16
+  - Byte order, BOM
+  - Конвертация из-в UTF-8
+- разбивать текст по байтам нельзя
+  - �
+  - да и по кодпоинтам нельзя
+  - Extended Grapheme Cluster
+  - «То, что воспринимается человеком как один символ»
+- Искать по подстроке нельзя
+  - Uppercase/lowercase
+  - Номализация
+  - NFD — все взорвать, é → e + ◌́
+  - NFC — все слепить, e + ◌́ → é
+  - macOS/Windows й
+  - X и 𝕏 (U+1D54F, MATHEMATICAL DOUBLE-STRUCK CAPITAL X)
+  - 1 и ¹ и ₁
+- Локааали
+  - uppercase/lowercase
+    - I → ı, i → İ
+  - Han unification
+    - For Japanese, the kanji characters have been unified with Chinese; that is, a character considered to be the same in both Japanese and Chinese is given a single number, even if the appearance is actually somewhat different, with the precise appearance left to the use of a locale-appropriate font
+  - Болгарица
+    - https://twitter.com/nikitonsky/status/1171115067112398849
+  - Text Segmentation
+    - Границы букв, слогов (для переносов), слов, предложений
+    - Работает через словарь
+    - Поэтому ICU весит 10 Мб, а не 10 Кб :(
+  - Новые версии каждый год
+    - Результаты разбиения/переноса могут поменяться
--- src/site/server.clj
+++ src/site/server.clj
@@ -1,29 +1,67 @@
 (ns site.server
   (:require
     [clj-simple-router.core :as router]
+    [clojure.java.io :as io]
     [mount.core :as mount]
     [org.httpkit.server :as http]
-    [ring.middleware.content-type :as content-type]
-    [ring.util.response :as response]
-    [site.parser :as parser]))
+    [ring.middleware.head :as ring-head]
+    [ring.util.codec :as ring-codec]
+    [ring.util.io :as ring-io]
+    [ring.util.mime-type :as ring-mime]
+    [ring.util.time :as ring-time]
+    [site.parser :as parser])
+  (:import
+    [java.io File]))
+
+(defn find-file ^File [path]
+  (let [file ^File (io/file path)]
+    (when (.exists file)
+      (let [file (if (.isDirectory file)
+                   (io/file file "index.html")
+                   file)]
+        (when (.exists file)
+          file)))))
+
+(defn wrap-files [handler dir]
+  (fn [req]
+    (if-some [file (find-file (str dir "/" (:uri req)))]
+      {:status 200
+       :body   file
+       :headers {"Content-Length" (.length file)
+                 "Last-Modified"  (ring-time/format-date (ring-io/last-modified-date file))
+                 "Content-Type"   (ring-mime/ext-mime-type (.getName file))}}
+      (handler req))))
+
+(defn wrap-decode-uri [handler]
+  (fn [req]
+    (handler (update req :uri ring-codec/url-decode))))
 
 (def app
   (->
     (router/router
       (router/routes
-        "GET /**" [path]
-        (response/file-response (str "_site/" path))
-        
         "GET /blog/*" [id]
         {:status  200
-         :headers {"Content-type" "text/html;charset=UTF-8"}
+         :headers {"Content-type" "text/html; charset=UTF-8"}
          :body    (-> (str "blog/" id "/index.md")
                     slurp
                     parser/parse
                     parser/transform
                     parser/page
-                    parser/render)}))
-    content-type/wrap-content-type))
+                    parser/render)}
+        
+        "GET /about" []
+        {:status 301
+         :headers {"Location" "/projects/"}}
+        
+        "GET /**" [path]
+        {:status  404
+         :headers {"Content-type" "text/plain; charset=UTF-8"}
+         :body    (str "Path '" path "' not found")}))
+    (wrap-files "_site")
+    (wrap-files "site")
+    wrap-decode-uri
+    ring-head/wrap-head))
 
 (def *server-opts
   (atom
