920736b06a3dc0a61d6cc757c862ba11468e1707
Nikita Prokopov <niki@tonsky.me>
2023-09-17 21:59:20 +0200

    sync posts modification times

--- script/sync.sh
+++ script/sync.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-set -o errexit -o nounset -o pipefail -o xtrace
+set -o errexit -o nounset -o pipefail
 cd "`dirname $0`/.."
+
+FILES=`find site/blog -name index.md`
+for file in $FILES; do
+    time="$(git log --pretty=format:%cd -n 1 --date=format:%Y%m%d%H%M.%S --date-order -- "$file")"
+    echo "changing modification time of" $file "to" $time
+    touch -m -t "$time" "$file"
+done
+
+touch -m -t 202207261436.26 site/blog/dice-out/index.md 
+touch -m -t 202303092055.37 site/blog/clojure-sublimed-3/index.md
+touch -m -t 202305031142.50 site/blog/humble-state/index.md
+touch -m -t 202305191428.55 site/blog/humble-signals/index.md 
+touch -m -t 202306291434.24 site/blog/clojurescript-2/index.md
 
 rsync --verbose --compress --recursive --times --delete _site site@tonsky.me:
 rsync --verbose --compress --recursive --times --delete site site@tonsky.me:
